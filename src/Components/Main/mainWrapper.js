5d615d37873432a6656c8d2bb647a26b937770ec
ChristianDev123 <christian.lima999@gmail.com>
2022-04-24 19:00:41 -0300

    Clean MainWrapper Component

--- src/Components/Main/mainWrapper.js
+++ src/Components/Main/mainWrapper.js
@@ -20,33 +20,23 @@ export default function MainWrapper() {
   const larguraTela = window.innerWidth
   return (
     <>
-      <main>
-        <Container fluid>
-          <FeaturedInfo larguraTela={larguraTela} dados = {coins} />
-        </Container>
-
-        <Container fluid>
-          <Row xs={1} lg={3} className={larguraTela < 992 ? "gap-4" : ''}>
-            <Col>
-              <Card larguraTela={larguraTela} dados = {coins}
-                />
-                
-            </Col>
-
-            <Col lg={8}>
-              <MainChart/>
-            </Col>
-          </Row>
-        </Container>
-        <Container fluid>
-          <WidgetInfo />
-        </Container>
-      </main>
-      <style jsx>{`
-      main{
-        position:absolute;
-      }
-    `}</style>
+      <Container fluid>
+        <FeaturedInfo larguraTela={larguraTela} dados = {coins} />
+      </Container>
+      <Container fluid>
+        <Row xs={1} lg={3} className={larguraTela < 992 ? "gap-4" : ''}>
+          <Col>
+            <Card larguraTela={larguraTela} dados = {coins}
+              />
+          </Col>
+          <Col lg={8}>
+            <MainChart/>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid>
+        <WidgetInfo />
+      </Container>
     </>
   );
 }
